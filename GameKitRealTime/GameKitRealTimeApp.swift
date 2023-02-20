f393c8fc4b10ae1d908cab286c3887085c2d9021
Zoe Cutler <zcutlerf@gmail.com>
2023-02-20 10:06:48 -0500

    Initial Commit

---
+++ GameKitRealTime/GameKitRealTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameKitRealTimeApp.swift
+//  GameKitRealTime
+//
+//  Created by Zoe Cutler on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct GameKitRealTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
